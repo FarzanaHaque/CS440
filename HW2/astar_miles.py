37eb1f47a83d6813719843314e0899f2966790c2
Alex <axchung2@illinois.edu>
2018-03-07 14:36:49 -0600

    buggy reflex

---
+++ HW2/astar_miles.py
@@ -0,0 +1,98 @@
+import queue
+
+miles_map = {"AB":1064,"AC":673,"AD":1401,"AE":277,"BC":958,"BD":1934,"BE":337,"CD":1001,"CE":399,"DE":387}
+
+def miles_so_far(str):
+	if len(str)==1:
+		return 0
+	mi = 0
+	for i in range(0,len(str)-1):
+		p = str[i:i+2]
+		if p in miles_map:
+			mi += miles_map[p]
+		else:
+			mi += miles_map[p[::-1]]
+	return mi
+	
+def alt_dist_est(str):     #because E doesn't obey triangle rule
+	if len(str)==1:
+		return 0
+	mi = 0
+	for i in range(0,len(str)-1):
+		p = str[i:i+2]
+		if p not in miles_map:
+			p = p[::-1]
+		if 'E' not in p:
+			if miles_map[p[0]+'E']+miles_map[p[1]+'E'] < miles_map[p]:
+				mi += miles_map[p[0]+'E']+miles_map[p[1]+'E']
+			else:
+				mi += miles_map[p]
+		else:
+			mi += miles_map[p]
+	return mi
+
+def avg_miles_remaining(str,widgets):
+	rem=0
+	for w in widgets:
+		tot=len(w)
+		ind=0
+		for s in str:
+			if tot==0:
+				break
+			if w[ind]==s:
+				tot-=1
+				ind+=1
+		if tot > 0:
+			rem += alt_dist_est(s[-1]+w[ind:])
+			
+	return rem / len(widgets)
+
+def h_g(str,widgets):
+	return avg_miles_remaining(str,widgets)+miles_so_far(str)
+
+def astar_miles(widgets):
+	q = queue.PriorityQueue()
+	q.put((h_g("A",widgets),"A"))
+	q.put((h_g("B",widgets),"B"))
+	q.put((h_g("D",widgets),"D"))
+	visited = set(["A","B","D"])
+	final=""
+	min_mi = float('inf')
+	while not q.empty():
+		node = q.get()
+		
+		if node[0]>=min_mi:
+			break
+			
+		if avg_miles_remaining(node[1],widgets)==0:
+			if node[0] < min_mi:
+				final=node[1]
+				min_mi=node[0]
+				continue
+		
+		if (node[1][-1]!="A") and (node[1]+"A") not in visited:
+			q.put((h_g(node[1]+"A",widgets),node[1]+"A"))
+			visited.add(node[1]+"A")
+		
+		if (node[1][-1]!="B") and (node[1]+"B") not in visited:
+			q.put((h_g(node[1]+"B",widgets),node[1]+"B"))
+			visited.add(node[1]+"B")
+		
+		if (node[1][-1]!="C") and (node[1]+"C") not in visited:
+			q.put((h_g(node[1]+"C",widgets),node[1]+"C"))
+			visited.add(node[1]+"C")
+		
+		if (node[1][-1]!="D") and (node[1]+"D") not in visited:
+			q.put((h_g(node[1]+"D",widgets),node[1]+"D"))
+			visited.add(node[1]+"D")
+		
+		if (node[1][-1]!="E") and (node[1]+"E") not in visited:
+			q.put((h_g(node[1]+"E",widgets),node[1]+"E"))
+			visited.add(node[1]+"E")
+		
+	return final
+		
+widg = ("AEDCA","BEACD","BABCE","DADBD","BECBD")
+sol = astar_miles(widg)
+print(sol)
+print(miles_so_far(sol))	
---
+++ HW2/gomoku.py
@@ -0,0 +1,261 @@
+#(column, row)
+#(i, j)
+
+
+class Board:
+    field = []
+    turn_number = 1
+    winner = '.'
+
+    def __init__(self):
+        for i in range(0, 7):
+            self.field.append([0, 0, 0, 0, 0, 0, 0])
+            for j in range(0, 7):
+                self.field[i][j] = '.'
+
+    def print_board(self):
+        print("  0 1 2 3 4 5 6")
+        for i in range(0, 7):
+            print(str(i) + " ", end="")
+            for j in range(0, 7):
+                print(str(self.field[i][j]) + " ", end="")
+            print("")
+
+
+class ReflexAgent:
+    mid = ''
+    oid = ''
+
+    def __init__(self, ID, oID):
+        self.mid = ID
+        self.oid = oID
+
+    def get_my_stones(self, b, cid):
+        my_stones = []
+        for i in range(0, 7):
+            for j in range(0, 7):
+                if b.field[i][j] == cid:
+                    my_stones.append((i, j))
+        return my_stones
+
+    def instant_win(self, b, cid):
+        my_stones = self.get_my_stones(b, cid)
+        for s in my_stones:
+            # check up
+            found_best = True
+            for count in range(1, 3 + 1):
+                cand = (s[0] - count, s[1])
+                if cand not in my_stones or s[0] - count < 0:
+                    found_best = False
+                    break
+            if found_best:
+                best = (s[0] - 4, s[1])
+                if s[0] - 4 >= 0 and b.field[best[0]][best[1]] == ".":
+                    return best
+
+            # check down
+            found_best = True
+            for count in range(1, 3 + 1):
+                cand = (s[0] + count, s[1])
+                if cand not in my_stones or s[0] + count > 6:
+                    found_best = False
+                    break
+            if found_best:
+                best = (s[0] + 4, s[1])
+                if s[0] + 4 <= 6 and b.field[best[0]][best[1]] == ".":
+                    return best
+
+            # check left
+            found_best = True
+            for count in range(1, 3 + 1):
+                cand = (s[0], s[1] - count)
+                if cand not in my_stones or s[1] - count < 0:
+                    found_best = False
+                    break
+            if found_best:
+                best = (s[0], s[1] - 4)
+                if s[1] - 4 >= 0 and b.field[best[0]][best[1]] == ".":
+                    return best
+
+            # check right
+            found_best = True
+            for count in range(1, 3 + 1):
+                cand = (s[0], s[1] + count)
+                if cand not in my_stones or s[1] + count > 6:
+                    found_best = False
+                    break
+            if found_best:
+                best = (s[0], s[1] + 4)
+                if s[1] + 4 <= 6 and b.field[best[0]][best[1]] == ".":
+                    return best
+
+        return -420, -420
+
+    def find_triple_chain_empty(self, b, cid):
+        my_stones = self.get_my_stones(b, cid)
+        for s in my_stones:
+            #check up
+            found_best = True
+            for count in range(1, 2 + 1):
+                cand = (s[0] - count, s[1])
+                if cand not in my_stones:
+                    found_best = False
+                    break
+            if found_best:
+                if s[0] + 1 < 6 and s[0] - 3 > 0:
+                    head1 = (s[0] + 1, s[1])
+                    head2 = (s[0] - 3, s[1])
+                    if b.field[head1[0]][head1[1]] == "." and b.field[head2[0]][head2[1]] == ".":
+                        return head1, head2
+
+            #check down
+            found_best = True
+            for count in range(1, 2 + 1):
+                cand = (s[0] + count, s[1])
+                if cand not in my_stones:
+                    found_best = False
+                    break
+            if found_best:
+                if s[0] - 1 > 0 and s[0] + 3 < 6:
+                    head1 = (s[0] - 1, s[1])
+                    head2 = (s[0] + 3, s[1])
+                    if b.field[head1[0]][head1[1]] == "." and b.field[head2[0]][head2[1]] == ".":
+                        return head2, head1
+
+            #check left
+            found_best = True
+            for count in range(1, 2 + 1):
+                cand = (s[0], s[1] - count)
+                if cand not in my_stones:
+                    found_best = False
+                    break
+            if found_best:
+                if s[1] + 1 < 6 and s[1] - 3 > 0:
+                    head1 = (s[0], s[1] + 1)
+                    head2 = (s[0], s[1] - 3)
+                    if b.field[head1[0]][head1[1]] == "." and b.field[head2[0]][head2[1]] == ".":
+                        return head1, head2
+
+            #check right
+            found_best = True
+            for count in range(1, 2 + 1):
+                cand = (s[0], s[1] + count)
+                if cand not in my_stones:
+                    found_best = False
+                    break
+            if found_best:
+                if s[1] - 1 > 0 and s[1] + 3 < 6:
+                    head1 = (s[0], s[1] - 1)
+                    head2 = (s[0], s[1] + 3)
+                    if b.field[head1[0]][head1[1]] == "." and b.field[head2[0]][head2[1]] == ".":
+                        return head2, head1
+        return -420, -420
+
+    def find_all_win_segs(self, b, oid):
+        win_segs = []
+        # look at right segs
+        for i in range(0, 7):
+            for j in range(0, 3):
+                # attempt to build win seg
+                win_seg = []
+                good_win = True
+                for count in range(0, 4 + 1):
+                    if b.field[i][j + count] != oid:
+                        win_seg.append((i, j+count))
+                    else:
+                        good_win = False
+                        break
+                if good_win:
+                    win_segs.append(win_seg)
+
+        # look at downward segs
+        for i in range(0, 3):
+            for j in range(0, 7):
+                # attempt to build win seg
+                win_seg = []
+                good_win = True
+                for count in range(0, 4 + 1):
+                    if b.field[i + count][j] != oid:
+                        win_seg.append((i + count, j))
+                    else:
+                        good_win = False
+                        break
+                if good_win:
+                    win_segs.append(win_seg)
+
+        return win_segs
+
+    def win_seg_move(self, b, mid, oid):
+        win_segs = self.find_all_win_segs(b, oid)
+        win_seg_count = {}
+
+        for ws in win_segs:
+            score = 0
+            for t in ws:
+                if b.field[t[0]][t[1]] == mid:
+                    score += 1
+            win_seg_count[tuple(ws)] = score
+
+        best_score = max(win_seg_count.values())
+        win_seg_best = [y for y in win_seg_count.keys() if win_seg_count[y] == best_score]
+        leftmost = min(win_seg_best, key=lambda t: t[1])[0]
+        leftmostvalues = [y for y in win_seg_best if y[0][1] == leftmost[1]]
+        downleftmost = max(leftmostvalues, key=lambda t: t[0])
+
+        for t in downleftmost:
+            if b.field[t[0]][t[1]] == '.':
+                return t
+
+        print("Error: win_seg_move has found a win seg that has no valid places")
+        return -420, -420
+
+    def make_move(self, b):
+        if b.turn_number == 1:
+            pass # can add a random later here for strategies
+        # Part 1
+        best_move = self.instant_win(b, self.mid)
+        if best_move != (-420, -420):
+            b.field[best_move[0]][best_move[1]] = self.mid
+            b.winner = self.mid
+            return
+
+        # Part 2
+        best_move = self.instant_win(b, self.oid)
+        if best_move != (-420, -420):
+            b.field[best_move[0]][best_move[1]] = self.mid
+            return
+
+        # Part 3
+        empty_triple = self.find_triple_chain_empty(b, self.oid)
+        if empty_triple != (-420, -420):
+            b.field[empty_triple[0][0]][empty_triple[0][1]] = self.mid
+            return
+
+        # Part 4
+        best_move = self.win_seg_move(b, self.mid, self.oid)
+        b.field[best_move[0]][best_move[1]] = self.mid
+        return
+
+def main():
+    b = Board()
+    print("Initial State")
+    print("------------------------------")
+    b.print_board()
+    print("")
+    player1 = ReflexAgent('R', 'B')
+    player2 = ReflexAgent('B', 'R')
+    while b.winner == '.':
+        print("Turn " + str(b.turn_number))
+        print("------------------------------")
+        player1.make_move(b)
+        player2.make_move(b)
+        b.print_board()
+        b.turn_number += 1
+        print("")
+    print("Finished! Winner is " + b.winner)
+    print("------------------------------")
+    b.print_board()
+
+
+if __name__ == "__main__":
+    main()
