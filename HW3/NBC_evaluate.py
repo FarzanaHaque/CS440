dff478990b8409dd978c756cafff378ab0ad2855
Alex <axchung2@illinois.edu>
2018-03-30 19:45:52 -0500

    "updated test "

--- HW3/NBC_evaluate.py
+++ HW3/NBC_evaluate.py
@@ -1,4 +1,6 @@
-
+import NBC_trainer as NBCt
+import data_parsing as dp
+import numpy as np
 
 def evaluate(answers, test_data):
 	"""
@@ -9,7 +11,21 @@ def evaluate(answers, test_data):
 			confusion matrix (10*10) (see webpage for details))
 	"""
 	# Calculate score
-	class_acc = [0] * 10
+	amount_answer_class = np.array([0] * 10)
+	amount_actual_class = np.array([0] * 10)
+	amount_correct_class = np.array([0] * 10)
+
 	for i in range(0, len(answers)):
-		pass
-	return None, None
+		amount_answer_class[answers[i][10]] += 1
+		amount_actual_class[test_data[i][dp.GET_ANSWER]] += 1
+		if answers[i][10] == test_data[i][dp.GET_ANSWER]:
+			amount_correct_class[answers[i][10]] += 1
+
+	relative_error = np.absolute(amount_answer_class - amount_actual_class) / amount_actual_class
+	class_acc = amount_correct_class / amount_actual_class
+
+	# Calculate Confusion Matrix
+	confusion_matrix = np.zeros((10,10))
+	for i in range(0, len(answers)):
+
+	return class_acc, None
