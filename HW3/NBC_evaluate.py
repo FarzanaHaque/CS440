b53fa6234cfcb0ccc04318ce2c963573c04a9a64
Alex <axchung2@illinois.edu>
2018-03-30 20:47:51 -0500

    "updated test "

--- HW3/NBC_evaluate.py
+++ HW3/NBC_evaluate.py
@@ -3,6 +3,36 @@ import data_parsing as dp
 import numpy as np
 
 
+def get_best_and_worst(answers, test_data, in_class):
+	"""
+	finds the tokens that have the highest probability and lowest probability
+	:param answers:
+	:param test_data:
+	:param in_class: the class
+	:return: [(2d img, best prob), (2d img, worst prob)]
+	"""
+	ret = [[None, 0.0], [None, 1.0]]
+	for i in range(0, len(answers)):
+		if test_data[i][dp.GET_ANSWER] == in_class:
+			if answers[i][in_class] > ret[0][1]:
+				# found maxer
+				ret[0][1] = answers[i][in_class]
+				ret[0][0] = test_data[i][dp.GET_IMAGE]
+			elif answers[i][in_class] < ret[1][1]:
+				# found miner
+				ret[1][1] = answers[i][in_class]
+				ret[1][0] = test_data[i][dp.GET_IMAGE]
+	return ret
+
+
+def print_best_and_worst(best_and_worst, in_class):
+	print("For class: " + str(in_class))
+	print("Highest Prob of " + str(best_and_worst[0][1]))
+	dp.print_image(best_and_worst[0][0])
+	print("Lowest Prob of " + str(best_and_worst[1][1]))
+	dp.print_image(best_and_worst[1][0])
+
+
 def evaluate(answers, test_data):
 	"""
 	Evals the answers that the NBC produces in testing
@@ -34,5 +64,10 @@ def evaluate(answers, test_data):
 		confusion_matrix[i, :] = confusion_matrix[i, :] / amount_actual_class[i]
 
 	# Print the most and least fit stuff for each image
+	best_and_worst_tokens = []
+	for i in range(0, len(answers)):
+		temp = get_best_and_worst(answers, test_data, i)
+		print_best_and_worst(temp, i)
+		best_and_worst_tokens.append(temp)
 
 	return class_acc, confusion_matrix
